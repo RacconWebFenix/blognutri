c5ae6c84427297ef87870604fca266afc42a0fd2
RacconWebFenix <dfenixweb@gmail.com>
2021-09-01 10:21:08 -0300

    Correção pra build

--- src/Pages/BlogInfo.js
+++ src/Pages/BlogInfo.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams, useHistory, Link } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import "./blogInfo.css";
 import api from "../Services/api";
 import { toast } from "react-toastify";
